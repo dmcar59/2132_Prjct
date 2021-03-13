8fccbffaea86e47f279affc273a9022c5916c8d5
dmcar59 <80501782+dmcar59@users.noreply.github.com>
2021-03-13 14:58:53 -0800

    Roll Completed

--- scripts/project.js
+++ scripts/project.js
@@ -1,29 +1,86 @@
 const popUp = document.getElementById('pop-up');
 const start = document.getElementById('start');
+const score = document.getElementById('score');
+const winner = document.getElementById('winner');
 
-let rollp = 0;
-let rollc = 0;
+let rollp1 = 0;
+let rollc1 =0;
+let rollp2 = 0;
+let rollc2 =0;
+let totalp = 0;
+let totalc =0;
+let subp = 0;
+let subc = 0;
 let counter = 0;
 
 start.addEventListener("click",function(){
+  /*Roll Dice*/
     if(counter < 2){
-        rollp = Math.floor(Math.random() * 6 + 1);
-        rollc = Math.floor(Math.random()*6+1);
-        console.log(rollp);
-        console.log(rollc);
+        rollp1 = Math.floor(Math.random() * 6 + 1);
+        rollp2 = Math.floor(Math.random()*6+1);
+        rollc1 = Math.floor(Math.random() * 6 + 1);
+        rollc2 = Math.floor(Math.random()*6+1);
+        console.log(rollp1, rollc1);
+        console.log(rollp2, rollc2);
         counter ++;
     }
     else if(counter <3){
-        rollp = Math.floor(Math.random() * 6 + 1);
-        rollc = Math.floor(Math.random()*6+1);
-        console.log(rollp);
-        console.log(rollc);
+        rollp1 = Math.floor(Math.random() * 6 + 1);
+        rollp2 = Math.floor(Math.random()*6+1);
+        rollc1 = Math.floor(Math.random() * 6 + 1);
+        rollc2 = Math.floor(Math.random()*6+1);
+        console.log(rollp1, rollc1);
+        console.log(rollp2, rollc2);
         counter ++;
-        popUp.style.opacity =1; 
+        popUp.style.opacity =1;
+        console.log(`roll counter ${counter}`); 
         console.log("finished")
-        return counter;
+       
     }
-   
+    else{
+        console.log('return');
+        return;
+    }
+   /*Add Dice Score*/
+   console.log('here I am');
+if(rollp1 == 0 || rollp2==0){
+totalp = 0;
+console.log(`one is zero = ${totalp}`);
+}
+else if(rollp1 == rollp2){
+    totalp = (rollp1 + rollp2)*2;
+    console.log(` x 2 = ${totalp}`);
+}
+else{
+    totalp = rollp1 + rollp2;
+    console.log(`regular ${totalp}`);
+}
+if(rollc1 == 0 || rollc2==0){
+    console.log(`one is zero = ${totalc}`);
+    totalc = 0;
+    }
+    else if(rollc1 == rollc2){
+        totalc = (rollc1 + rollc2)*2;
+        console.log(` x 2 = ${totalc}`);
+    }
+    else{
+        totalc = rollc1 + rollc2;
+        console.log(`regular ${totalc}`);
+    }
+subp += totalp;
+subc += totalc;
+score.innerHTML = `<p> Player score: ${subp} and Computer score: ${subc}`; 
+console.log(`player subtotal ${subp} and computer subtotal ${subc}`);
+
+if(counter == 3 && subp>subc){
+winner.innerHTML = `Player Wins`;
+}
+else if(counter ==3 && subp<subc){
+    winner.innerHTML = `Computer Wins`;
+}
+else{
+    winner.innerHTML = `Its a Tie!`;
+}
 })  
 
 
